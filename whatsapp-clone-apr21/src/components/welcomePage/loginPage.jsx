a143a0b13d25691d7c2b6998aca99a64ae04659a
Helena Nel <mailhedri@gmail.com>
2021-09-06 13:56:42 +0200

    commit

---
+++ whatsapp-clone-apr21/src/components/welcomePage/loginPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  Container,
+  Card,
+  Row,
+  Column,
+  Button,
+  Form,
+  FormControl,
+} from "react-bootstrap";
+const loginPage = () => {
+  <>
+    <Container>
+      <Card>
+        <Card.Text>Helloi</Card.Text>
+      </Card>
+    </Container>
+  </>;
+};
+
+export default loginPage;
