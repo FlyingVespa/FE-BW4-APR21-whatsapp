72705063ab077897e26771c3e3a2f0e382e774f3
Helena Nel <mailhedri@gmail.com>
2021-09-07 09:34:42 +0200

    Update MainUser.jsx

--- whatsapp-clone-apr21/src/components/LeftSidebar/MainUser.jsx
+++ whatsapp-clone-apr21/src/components/LeftSidebar/MainUser.jsx
@@ -3,3 +3,7 @@ import React from "react";
 export default function MainUser() {
   return <div></div>;
 }
+// THE LOGGED IN USER SHOULD BE DISPLAYED HERE
+// TO SHOW PICTURE
+// UPLOAD CHANGE PICTURE
+// UPDATE ADD STATUS OF USER
