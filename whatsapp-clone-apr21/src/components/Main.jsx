727b8775a45db0d235cf54bd70a449c8c4c26026
Helena Nel <mailhedri@gmail.com>
2021-09-07 19:36:00 +0200

    commit

--- whatsapp-clone-apr21/src/components/Main.jsx
+++ whatsapp-clone-apr21/src/components/Main.jsx
@@ -2,23 +2,55 @@ import React from "react";
 import LeftSidebar from "./LeftSidebar/LeftSidebar";
 import RightSidebar from "./RightSidebar/RightSidebar";
 import CenterSection from "./MainSection/CenterSection";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Button } from "react-bootstrap";
+import { useState } from "react";
 
 const Main = () => {
+  const [showRightbar, setShowRightbar] = useState(false);
+
   return (
     <Container className="mainpage_container">
+      <Button
+        onClick={() => {
+          setShowRightbar(true);
+        }}
+      >
+        Show Right
+      </Button>
+      <Button
+        variant="danger"
+        onClick={() => {
+          setShowRightbar(false);
+        }}
+      >
+        Close Right
+      </Button>
       <Row>
-        <Col className="p-0" xs={3}>
-          <LeftSidebar />
-        </Col>
-        <Col className="p-0" xs={6}>
-          <CenterSection />
-        </Col>
-        <Col className="p-0" xs={3}>
-          <RightSidebar />
-        </Col>
+        {!showRightbar ? (
+          <>
+            <Col className="p-0" xs={3}>
+              <LeftSidebar />
+            </Col>
+            <Col className="p-0" xs={9}>
+              <CenterSection />
+            </Col>
+          </>
+        ) : (
+          <>
+            <Col className="p-0" xs={3}>
+              <LeftSidebar />
+            </Col>
+            <Col className="p-0" xs={6}>
+              <CenterSection />
+            </Col>
+            <Col className="p-0" xs={3}>
+              <RightSidebar />
+            </Col>
+          </>
+        )}
       </Row>
     </Container>
   );
 };
+
 export default Main;
